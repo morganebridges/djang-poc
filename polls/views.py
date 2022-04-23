b7ab99c7109b5bdc5238518666cdc7d2207e3104
Morgan Bridges <morganebridges@gmail.com>
2022-04-23 17:27:55 -0500

    adding client testing, fixing views bugs

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
-from .models import Question, Choice
+
+from .models import Choice, Question
+
 
 # Create your views here.
 def vote(request, question_id):
@@ -26,12 +28,18 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         #  Return the last five published questions.
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        )
 
 class ResultsView(generic.DetailView):
     model = Question
